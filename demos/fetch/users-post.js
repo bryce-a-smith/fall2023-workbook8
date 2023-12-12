b75e1605574efeaf06460463a896df9c21c44ffc
BRYCE SMITH <a.bryce.smith.gmail.com>
2023-12-12 10:38:29 -0500

    fetch user post

---
+++ demos/fetch/users-post.js
@@ -0,0 +1,18 @@
+const user = {
+  name: "Lizzo Be Eating",
+  username: "thereallizzo",
+  email: "lizzo2245@gmail.com",
+};
+
+fetch("http://localhost:3000/users", {
+  method: "POST",
+  headers: {
+    "Content-Type": "application/json",
+    //can set multiple headers here
+  },
+  body: JSON.stringify(user),
+})
+  .then((response) => response.json())
+  .then((user) => {
+    console.log(user);
+  });
