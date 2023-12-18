ea013064524935d3d39f10144569223829c68578
BRYCE SMITH <a.bryce.smith.gmail.com>
2023-12-18 10:06:15 -0500

    fetch review

--- review/fetch.js
+++ review/fetch.js
@@ -1,1 +1,24 @@
-//GET
+//GET ALL
+fetch("https://myapi.com/presents");
+//OR
+fetch("https://myapi.com/presents", { method: "GET" });
+
+//GET ONE
+fetch("https://myapi.com/presents/1");
+
+//POST
+fetch("https://myapi.com/presents", {
+  method: "POST",
+  headers: { "Content-Type": "application/json" },
+  body: JSON.stringify(data),
+});
+
+//PUT
+fetch("https://myapi.com/presents/1", {
+  method: "PUT",
+  headers: { "Content-Type": "application/json" },
+  body: JSON.stringify(data),
+});
+
+//DELETE
+fetch("https://myapi.com/presents/1", { method: "DELETE" });
